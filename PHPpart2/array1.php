1bc3cb1a90b668e0a6c2657e3eeb293a1032bcb7
Muhfarq <mhmmadfarq7@gmail.com>
2024-09-24 08:43:32 +0700

    Praktikum1

---
+++ PHPpart2/array1.php
@@ -0,0 +1,17 @@
+<html>
+    <body>
+        <h2>Array Index</h2>
+        <?php
+        $listDosen=["Elok Nur Hamdana", "Unggul Pamecang", "Bagas Nugraha" ];
+
+        //echo $listDosen[2] . "<br>";
+        //echo $listDosen[0] . "<br>";
+        //echo $listDosen[1] . "<br>";
+
+        foreach ($listDosen as $Dosen) {
+            echo  $Dosen . "<br>";
+
+        }
+        ?>
+    </body>
+</html>
