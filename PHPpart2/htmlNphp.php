e3eadcfb7bc25fce28c84f1b1e791102c8d66c50
Muhfarq <mhmmadfarq7@gmail.com>
2024-09-26 11:40:53 +0700

    practikal 8

---
+++ PHPpart2/htmlNphp.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+    </head>
+    <h3>Cara 1</h3>
+    <body>
+        <p>Tanggal Hari ini : <?php echo date("D m M Y")?></p>
+    </body>
+</html>
+
+<?php
+echo "<h3>Cara 2</h3>";
+echo '<html>';
+echo '<body>';
+echo '<p>Tanggal hari ini : '.date('D m M Y').'</p>';
+echo '<body>';
+echo '<html>';
+
+
+
+?>
