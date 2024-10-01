379377543556961ac1a8a0f872d56425ef506eec
Muhfarq <mhmmadfarq7@gmail.com>
2024-10-01 08:46:21 +0700

    praktikum 2

---
+++ JQUERRY/praktikJQuery.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Praktikum JQuery</title>
+        <script type= "text/javascript" src="jquery-3.7.1.js"></script>
+        <script>
+            $(document) .ready(function(){
+                $("#button1").click(function(){
+                    alert("ini praktikum minggu ke-4");
+            })
+        })
+        </script>
+    </head>
+    <input type="button" id="button1" value="klik saya"/>
+</html>
+
+
+
+
