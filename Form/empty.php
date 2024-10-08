f9a96c65fc81fc761de398b50e06cb305c6d6cd4
= <=>
2024-10-08 08:53:15 +0700

    praktikum 2 - JS7

---
+++ Form/empty.php
@@ -0,0 +1,14 @@
+<?php
+$myArray = array(); //array kosong
+if (empty($myArray)) {
+    echo "Array kosong <br>";
+} else {
+    echo "Array tidak kosong";
+}
+
+if (empty($nonExixtentVar)) {
+    echo "Variable tidak ada";
+} else {
+    echo "variable terdevinisi";
+}
+?>
