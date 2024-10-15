c273496996f574125a1631c1d7eb60047c889dc6
Muhfarq <mhmmadfarq7@gmail.com>
2024-10-15 19:01:46 +0700

    practikum 10 js 8

---
+++ PHP3/homeSession.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        <?php
+        session_start();
+        if (isset($_SESSION['status']) && $_SESSION['status'] == 'login') {
+            echo "Selamat datang, " . $_SESSION['username'];
+        ?>
+        <br><a href="sessionLogout.php">Logout</a>
+        <?php
+        } else {
+            echo "Anda belum login, silahkan ";
+        ?>
+        <a href="sessionLoginForm.html">Login</a>
+        <?php
+        }
+        ?>
+    </body>
+</html>
