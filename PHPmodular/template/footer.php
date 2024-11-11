72976cdb84536a9b9b0c0b09291040eb4fba1f5e
Muhfarq <mhmmadfarq7@gmail.com>
2024-11-11 14:41:06 +0700

    percobaan 3 js 11

---
+++ PHPmodular/template/footer.php
@@ -0,0 +1,5 @@
+<script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLISHBJdDIISYN/njoSsxfbrkCTnJrzXt+ENPSMOVBxD+165EG4zoLp" crossorigin="anonymous"></script>
+<script src="assets/custom/dashboard.js"></script>
+</body>
+</html>
