4426bd905eeddb3ad0117561c2501c4fa5acf57c
Muhfarq <144863786+Muhfarq@users.noreply.github.com>
2024-11-26 08:39:47 +0700

    Delete BoostrapCrud/crud/ajax directory

--- BoostrapCrud/crud/ajax/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Dummy authentication check
-if (!isset($_SESSION['user_logged_in'])) {
-    header('Location: login.php'); // Redirect to login if not logged in
-    exit();
-}
-?>
